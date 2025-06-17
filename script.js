4139e6084d304f60ab5bd922d7e91b406fba9248
Jayswal  Ajay <118416940+Ajay3010@users.noreply.github.com>
2025-06-17 13:46:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+  let count = 0;
+  for (let i = 5; n / i >= 1; i *= 5) {
+    count += Math.floor(n / i);
+  }
+  return count;
 }
 
 const input = prompt("Enter a number");
-alert(trailingZeros(input));
+const number = parseInt(input);
+
+if (!isNaN(number) && number >= 0) {
+  alert(trailingZeros(number));
+} else {
+ // alert("Please enter a valid non-negative integer.");
+}
+
